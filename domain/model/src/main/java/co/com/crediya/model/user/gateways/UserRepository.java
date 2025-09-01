2c7595b13499db5640550c51bc4d57164f22e392
ivan <ivizcain4@cuc.edu.co>
2025-09-01 12:11:26 -0500

    feat(domain): add user repository port reative methods

--- domain/model/src/main/java/co/com/crediya/model/user/gateways/UserRepository.java
+++ domain/model/src/main/java/co/com/crediya/model/user/gateways/UserRepository.java
@@ -1,19 +1,39 @@
 package co.com.crediya.model.user.gateways;
 
-import java.util.List;
-
 import co.com.crediya.model.user.User;
+import co.com.crediya.model.valueobject.Email;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 
 public interface UserRepository {
-
-    void saveUser(User user);
-
-    List<User> getAllUsers();
-
-    User getUserById(String id);
-
-    User getUserByEmail(String Email);
-
-    void deleteUSer(User user);
     
+    /**
+     * Saves a new user into the repository
+     * @param user User to be saved
+     * @return Mono with the saved user
+     */
+    Mono<User> save(User user);
+
+    Flux<User> findAll();
+
+    /**
+     * Finds a user by their email address
+     * @param email Email address to search
+     * @return Mono with the found user or empty if not found
+     */
+    Mono<User> findByEmail(Email email);
+    
+    /**
+     * Finds a user by their ID
+     * @param id User ID
+     * @return Mono with the found user or empty if not found
+     */
+    Mono<User> findById(String id);
+    
+    /**
+     * Checks if a user with the given email already exists
+     * @param email Email address to verify
+     * @return Mono with true if exists, false otherwise
+     */
+    Mono<Boolean> existsByEmail(Email email);
 }
